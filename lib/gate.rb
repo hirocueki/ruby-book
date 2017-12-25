52d537d95fa322c5482b5fd41f8dded09d24b05f
hirocueki <hirocueki@gmail.com>
2017-12-25 17:43:23 +0900

    Add RDoc

--- lib/gate.rb
+++ lib/gate.rb
@@ -1,15 +1,27 @@
+# Gate class
 class Gate
     STATIONS = [:umeda, :juso, :mikuni]
     FARES = [150,190]
 
+    # Initialize Gate object
+    # === Arguments
+    # * +name+ - Station name
     def initialize(name)
         @name = name
     end
 
+    # Enter to Station
+    # === Arguments
+    # * +ticket+ - Ticket
     def enter(ticket)
         ticket.stamp(@name)
     end
 
+    # Exit from Station
+    # === Arguments
+    # * +ticket+ - Ticket
+    # === Return Value
+    # * +boolean+ - true fare is enough, false fare is not enough
     def exit(ticket)
         fare = calc_fare(ticket)
         fare <= ticket.fare
