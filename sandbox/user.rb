1fa21fab8a7feee364d13b28e84cdd949e2c23ac
hirocueki <hirocueki@gmail.com>
2017-12-25 16:58:29 +0900

    Add clas method in user

--- sandbox/user.rb
+++ sandbox/user.rb
@@ -1,24 +1,43 @@
 class User
     attr_reader :first_name, :last_name, :age
 
-    def initialize(first_name, last_name, age)
+    def initialize(first_name)
         @first_name = first_name
-        @last_name = last_name
-        @age = age
     end
+    
+    # def initialize(first_name, last_name, age)
+    #     @first_name = first_name
+    #     @last_name = last_name
+    #     @age = age
+    # end
 
     def full_name
         "#{first_name} #{last_name}"
     end
+
+    def hello
+        puts "Hello, I am #{first_name}."
+        # full_name.chars.shuffle.join
+    end
+
+    def self.create_user(names)
+        names.map do | name |
+            User.new(name)
+        end
+    end
+
 end
 
 
-users = []
-users << User.new('Alice', 'Ruby', 20)
-users << User.new('Bob', 'Python', 30)
-
-
-
+users = User.create_user(%w(Alice Bob Carol))
 users.each do |user|
-    puts "氏名: #{user.full_name}、年齢 #{user.age}"
+    user.hello
 end
+
+# users = []
+# users << User.new('Alice', 'Ruby', 20)
+# users << User.new('Bob', 'Python', 30)
+# users.each do |user|
+#     puts "氏名: #{user.full_name}、年齢 #{user.age}"
+#     puts user.hello
+# end
