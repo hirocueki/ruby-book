de1b664bcb10c5cc9c484311cb2529412a0afa3a
hirocueki <hirocueki@gmail.com>
2017-12-26 17:20:57 +0900

    Create Loggable module

--- sandbox/module.rb
+++ sandbox/module.rb
@@ -1,25 +1,27 @@
-class Product
-    def title
-        log 'title is called.'
-        'A greate movie'
-    end
-
+# ログ出力用のメソッドを提供するモジュール
+module Loggable
     private
-
     def log(text)
         puts "[LOG] #{text}"
     end
 end
 
+class Product
+    # moduleをincludeする
+    include Loggable
+
+    def title
+        log 'title is called.'
+        'A greate movie'
+    end
+end
+
 class User
+    include Loggable
+
     def name
         log 'name is called.'
         'Alice'
-    end
-
-    private
-    def log(text)
-        puts "[LOG] #{text}"
     end
 end
 
@@ -27,3 +29,4 @@ product = Product.new
 product.title
 user = User.new
 user.name
+# user.log 'outsider log'
