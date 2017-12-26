6436aef14d4852cb3bed9fdecc0742a385dc67f4
hirocueki <hirocueki@gmail.com>
2017-12-26 18:23:12 +0900

    Add Refine example

---
+++ sandbox/refinements.rb
@@ -0,0 +1,19 @@
+module StringShuffle
+    refine String do
+        def shuffle
+            chars.shuffle.join
+        end
+    end
+end
+
+class User
+    using StringShuffle
+
+    def initialize(name)
+        @name = name
+    end
+
+    def shuffled_name
+        @name.shuffle
+    end
+end
