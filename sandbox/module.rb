e7d466cd88f2569088f290453cc5be25fbc2b8c7
hirocueki <hirocueki@gmail.com>
2017-12-26 17:12:42 +0900

    Add module example

---
+++ sandbox/module.rb
@@ -0,0 +1,29 @@
+class Product
+    def title
+        log 'title is called.'
+        'A greate movie'
+    end
+
+    private
+
+    def log(text)
+        puts "[LOG] #{text}"
+    end
+end
+
+class User
+    def name
+        log 'name is called.'
+        'Alice'
+    end
+
+    private
+    def log(text)
+        puts "[LOG] #{text}"
+    end
+end
+
+product = Product.new
+product.title
+user = User.new
+user.name
